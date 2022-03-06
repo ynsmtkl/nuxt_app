31b210fbd92059a49648830a043c7f9e87901272
Youness <yns.mtkl@gmail.com>
2022-03-06 13:49:02 +0100

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand :to="'/'">NavBar</b-navbar-brand>
+
+      <b-navbar-nav>
+        <b-nav-item :to="'/brands'">Brands</b-nav-item>
+        <b-nav-item :to="'/statistics'">Statistics</b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
+      <Nuxt/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "default"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/brands.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Brands</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "brands"
+}
+</script>
+
+<style scoped>
+
+</style>
